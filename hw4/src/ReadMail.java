768663573f979ac77bdb7b87d8b4daf48a512e86
Mark losev <moshe.losev@yahoo.com>
2017-11-04 23:14:57 -0400

    adding some imports

--- hw4/src/ReadMail.java
+++ hw4/src/ReadMail.java
@@ -1,5 +1,46 @@
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Part;
+import javax.mail.Session;
+import javax.mail.Store;
+
 public class ReadMail implements IReadMail {
     public EmailMessage[] readMail() {
+        Properties properties = new Properties();
+        properties.put("mail.store.protocol", "pop3");
+        properties.put("mail.pop3.host", pop3Host);
+        properties.put("mail.pop3.port", "995");
+        properties.put("mail.pop3.starttls.enable", "true");
+        Session emailSession = Session.getDefaultInstance(properties);
 
+        Store store = emailSession.getStore("pop3s");
+
+        store.connect(pop3Host, user, password);
+
+        Folder emailFolder = store.getFolder("INBOX");
+        emailFolder.open(Folder.READ_ONLY);
+        Message[] messages = emailFolder.getMessages();
+        EmailMessage[] contents = new EmailMessage[messages.length];
+
+        for (int i = 0; i < messages.length; i++) {
+            Message message = messages[i];
+
+        }
     }
 }
