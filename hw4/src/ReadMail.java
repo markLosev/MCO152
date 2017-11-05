b551d6c944eaf4b8371c54f20f981cc9cc20a3ff
Mark losev <moshe.losev@yahoo.com>
2017-11-05 15:01:12 -0500

    finished program

--- hw4/src/ReadMail.java
+++ hw4/src/ReadMail.java
@@ -1,12 +1,6 @@
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+
 import java.util.Date;
 import java.util.Properties;
 
@@ -14,9 +8,7 @@ import javax.mail.Address;
 import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Multipart;
 import javax.mail.NoSuchProviderException;
-import javax.mail.Part;
 import javax.mail.Session;
 import javax.mail.Store;
 
