bed739f7cc1ced96ca96e32a45a71a32e485f314
ygruen2 <ygruen2@student.touro.edu>
2017-11-07 22:28:15 -0500

    finished Contains Method Tests

---
+++ hw5/Tests/hw5/SecondSetOfTests.java
@@ -0,0 +1,21 @@
+package hw5;
+
+import org.junit.Before;
+
+import java.util.LinkedList;
+
+import static org.junit.Assert.*;
+
+public class SecondSetOfTests {
+
+    LinkedList<Integer> list;
+    Integer number = 1;
+
+
+    @Before
+    public void setUp() throws Exception {
+        list = new LinkedList<>();
+
+    }
+
+}
