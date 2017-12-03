c8ae4d88bb955b37e47179523b69f4d52f54b71c
Mark losev <moshe.losev@yahoo.com>
2017-12-03 17:17:12 -0500

    downloaded apache

--- hw6/src/TimeServerClock.java
+++ hw6/src/TimeServerClock.java
@@ -1,8 +1,29 @@
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.time.LocalDateTime;
+import java.net.InetAddress;
+import java.util.Date;
+import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.TimeInfo;
 
 public class TimeServerClock implements IClock{
     @Override
     public LocalDateTime now() {
-
+        String TIME_SERVER = "time-a.nist.gov";
+        NTPUDPClient timeClient = new NTPUDPClient();
+        InetAddress inetAddress = null;
+        try {
+            inetAddress = InetAddress.getByName(TIME_SERVER);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        TimeInfo timeInfo = null;
+        try {
+            timeInfo = timeClient.getTime(inetAddress);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        long returnTime = timeInfo.getMessage().getTransmitTimeStamp().getTime();
+        Date time = new Date(returnTime);
     }
 }
